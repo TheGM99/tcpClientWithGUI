e191b4dff8b55ec6b9266c120a332a3cda95b84a
TheGM99 <grzesmatczak@gmail.com>
2020-11-27 20:45:30 +0100

    Add project files.

---
+++ tcpClientWithGUI/FuncForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace tcpClientWithGUI
+{
+    public partial class FuncForm : Form
+    {
+        Form otherform;
+        public FuncForm(Form form)
+        {
+            InitializeComponent();
+            otherform = form;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Nothing was done.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            otherform.Show();
+        }
+    }
+}
